58923fdee9ced9d6ac61c825ff466389b484da13
Josh Koberstein <josh.koberstein@gmail.com>
2017-01-06 15:46:11 -0800

    Strict comparison

--- src/main/PhpJsonMarshaller/Marshaller/JsonMarshaller.php
+++ src/main/PhpJsonMarshaller/Marshaller/JsonMarshaller.php
@@ -236,7 +236,7 @@ class JsonMarshaller
         $result = null;
 
         // Decode the value into our result
-        if ($value == null) {
+        if ($value === null) {
             $result = $value;
         } elseif ($propertyType->getType() === PropertyTypeObject::TYPE_SCALAR) {
             $result = $propertyType->getValue()->decodeValue($value);
